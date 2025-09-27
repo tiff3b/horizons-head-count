f97a501dac641e2462ec3e2e211a3f998d2a22f1
tiff3b <tiffany3berger@gmail.com>
2025-09-27 13:45:41 -0400

    Added save button and styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+    console.log(countEl)
+
 let count = 0
 
-
 function server(){
-    count = count + 1
-    console.log(count)
+    count += 1
+    countEl.textContent= count
 }
 
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+    console.log(count)
+}
