959915df5797e183e44c46fbd69686d839424ec7
tiff3b <tiffany3berger@gmail.com>
2025-10-04 16:03:05 -0400

    Activated add button

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
+const addWaitress = document.querySelector(".add-staff")
+let addInputLabel = document.querySelector(".add-server label")
+let addInput = document.querySelector(".add-server input")
+let waitStaff = document.querySelector(".waitstaff")
+let serverButton = document.querySelector("#server-button")
 let countEl = document.getElementById("count-el")
 let saveEl = document.getElementById("save-el")
     console.log(countEl)
 
 let count = 0
+
+addWaitress.addEventListener("click", function (){
+    let onFloor = addInput.value;
+    if (onFloor !==""){
+        addToFloor(onFloor);
+        clearFloor();
+        //updateFloorPlan();
+    }
+});
+
+let clearFloor = function (){
+    addInput.value = "";
+};
+
+let addToFloor = function (onFloor){
+    let serverList = document.createElement("li");
+    serverList.innerText = onFloor;
+    waitStaff.append(serverList);
+};
 
 function server(){
     count += 1
